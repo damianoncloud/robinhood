f247403cebe9413eb52791c56a53ea711a11adf6
Damiano Del Bello <dam6dam@gmail.com>
2020-12-10 10:28:42 +0100

    Newsfeed sections and Chip done

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Newsfeed.css';
 import LineGraph from './LineGraph.js';
 import TimeLine from './TimeLine.js';
+import Chip from '@material-ui/core/Chip';
+import { Avatar } from '@material-ui/core';
+
 
 export default function Newsfeed() {
+
+    const [popularTopics, setTopics] = useState([
+        "Technology",
+        "Top Movies",
+        "Upcoming Earnings",
+        "Crypto",
+        "Cannabis",
+        "Healthcare Supplies",
+        "Index ETFs",
+        "Technology",
+        "China",
+        "Pharma",
+    ]);
+
     return (
         <div className="Newsfeed">
             <div className="newsfeed__container">
@@ -15,6 +32,34 @@ export default function Newsfeed() {
                     <div className="newsfeed__chart">
                         <LineGraph />
                         <TimeLine />
+                    </div>
+                </div>
+                <div className="newsfeed__buying__section">
+                    <h2> Buying Power</h2>
+                    <h2> $ 4.11</h2>
+                </div>
+                <div className="newsfeed__market__section">
+                    <div className="newsfeed__market__box">
+                        <p> Markets closed</p>
+                        <h1> Happy Thanksgiving</h1>
+                    </div>
+                </div>
+                <div className="newsfeed__popularlists__section">
+                    <div className="newsfeed__popularlists__intro">
+                        <h1>Popular lists</h1>
+                        <p>Show More</p>
+                    </div>
+                    <div className="newsfeed_popularlists_badges">
+                        {popularTopics.map((topic) => (
+                            <Chip
+                                className="topic__badge"
+                                variant="outlined"
+                                label={topic}
+                                avatar={<Avatar
+                                    src={`https://avatars.dicebear.com/api/human/${topic}.svg`}
+                                />}
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
